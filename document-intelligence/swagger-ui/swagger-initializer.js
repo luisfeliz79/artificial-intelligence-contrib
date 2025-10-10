8a74c52511a1b286cee5b5c3612f0217553f0edc
Luis <6314335+luisfeliz79@users.noreply.github.com>
2025-10-10 01:31:28 -0400

    updates

--- document-intelligence/swagger-ui/swagger-initializer.js
+++ document-intelligence/swagger-ui/swagger-initializer.js
@@ -4,8 +4,8 @@ window.onload = function() {
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
-      {url: "../specs/di-specs-v4.0-2024-11-30.yaml", name: "Document Intelligence v4.0"},
-      {url: "../specs/di-specs-v3.1-2023-10-31-preview.yaml", name: "Document Intelligence v3.1"}
+      {url: "../specs/di-specs-v4.0-2024-11-30.json", name: "Document Intelligence v4.0"},
+      {url: "../specs/di-specs-v3.1-2023-10-31-preview.json", name: "Document Intelligence v3.1"}
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
